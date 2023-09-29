03e6ff2e08de6c39114a7264d06085f8efc84419
Ball-slobber1 <bertrambendsen.bb@gmail.com>
2023-09-29 08:50:55 +0200

    Create NAC-Bot.py

---
+++ NAC-Bot.py
@@ -0,0 +1,41 @@
+import discord
+
+board = [[":green_square:", ":green_square:", ":green_square:"],
+[":green_square:", ":green_square:", ":green_square:"],
+[":green_square:", ":green_square:", ":green_square:"]]
+
+def boardprint():
+  for i in board:
+     print(i)
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+TOK_FILE = "token.txt"
+
+fav_emojis = {}
+reacts = {}
+
+def get_token():
+  tokfile = open(TOK_FILE, 'r')
+  token = tokfile.read()
+  tokfile.close()
+  return token
+
+@client.event
+async def on_ready():
+    print("Connected!")
+
+@client.event
+async def on_message(message):
+    contents = message.content
+    user_id = message.author.id
+    
+    if contents.startswith("!nac.play"):
+        reply1 = "Here's your board"
+        await message.channel.send(reply1)
+        await message.channel.send(boardprint())
+        
+token = get_token()
+client.run(token)

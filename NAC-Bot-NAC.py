ae1d7f7f97f51d016f9c033096e641557d21915d
Ball-slobber1 <bertrambendsen.bb@gmail.com>
2023-10-03 08:11:11 +0200

    Update NAC-Bot-NAC.py

--- NAC-Bot-NAC.py
+++ NAC-Bot-NAC.py
@@ -1,8 +1,8 @@
 import discord
 
-board = [[":green_square:", ":green_square:", ":green_square:"],
-[":green_square:", ":green_square:", ":green_square:"],
-[":green_square:", ":green_square:", ":green_square:"]]
+board = [[":one:", ":two:", ":three:"],
+[":four:", ":five:", ":six:"],
+[":seven:", ":eight:", ":nine:"]]
 
 
 intents = discord.Intents.default()
@@ -21,6 +21,10 @@ def get_token():
   tokfile.close()
   return token
 
+async def print_board(channel):
+   for i in board:
+      await channel.send(" ".join(i))
+
 @client.event
 async def on_ready():
     print("Connected!")
@@ -28,19 +32,23 @@ async def on_ready():
 @client.event
 async def on_message(message):
     contents = message.content
+    channel = message.channel
     user_id = message.author.id
     
     if contents.startswith("!nac.play"):
         reply1 = "Here's your board"
         await message.channel.send(reply1)
-        for i in board:
-          await message.channel.send(" ".join(i))
-    
-    play = True
-
-    while play == True:
-       await message.channel.send("Player1, enter row and coloumn numbers:")
-       play == False
+        await print_board(channel)
         
+        play = True
+    
+    while play == True:
+       await message.channel.send("Player1, enter the number you want to place your cross:")
+       
+       if contents.startswith("1"):
+          board[0][0] = ":x:"
+          await print_board(channel)
+          break
+      
 token = get_token()
 client.run(token)
